ca777e14bfd932eaa8d62cc98a41412913011884
Saul <saul.moro.gomez@gmail.com>
2022-03-21 16:16:33 +0100

    initial commit

---
+++ apps/ahlcg-tools/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ahlcg-tools-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'ahlcg-tools';
+}
